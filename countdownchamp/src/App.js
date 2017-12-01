b6a1f71ccf6033dcd6f66fff0120a4defbacfaee
kamoltip <plawansusu@gmail.com>
2017-12-01 02:35:12 -0500

    initial commit

---
+++ countdownchamp/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render(){
+    return(
+    <div>
+        <div>Countdown to December 25,2017</div>
+    </div>
+  )
+  }
+}
+
+export default App;
